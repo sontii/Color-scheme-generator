fdcbf37bd1a8055a1434fcf6292ca10062c9a63d
sontii <sontii78@gmail.com>
2022-12-17 12:30:48 +0100

    done

--- index.js
+++ index.js
@@ -4,34 +4,45 @@ const colorschema = document.getElementById('color-schema-select');
 const colorContainer = document.getElementById("color-container");
 const namedColorContainer = document.getElementById("named-color-container");
 
-const colors = []
+let colors = []
 
 document.getElementById('wrapper').addEventListener('click', (e) => {
     if(e.target.id === 'btn') {
         e.preventDefault()
-        getApiData(colorPicker.value.substring(1), colorschema.value);
-    }
+        const colorValue = colorPicker.value.substring(1);
+        getApiData(colorValue, colorschema.value);
+    } else if (e.target.className === "color" || e.target.className === "named-color") {
+			navigator.clipboard.writeText(e.target.innerText);
+		}
 })
 
-function getApiData(colorValue, mode) {   
+function getApiData(colorValue, mode) { 
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${mode}`)
         .then(res => res.json())
         .then(data => {
-                data.colors.map(hexValue => {
-                    colors.push(hexValue.hex.value)
-                })
+            data.colors.map(hexValue => {
+                colors.push(hexValue.hex.value)
+            })
+        render();
         });
-
-    render()
+    
 }
 
 function render() {
-    let colorHtml = ''
-    let namedColorHtml = ''
-    colors.map(color => {
-        colorHtml += `<div class"color"style="background-color:˘${color};"></div>`;
-        namedColorHtml += `<div class"named-color">${color};" </div>`;
-    })
-    colorContainer = colorHtml
-    namedColorContainer = namedColorHtml
+    let colorHtml = ""
+	let namedColorHtml = ""
+	colors.map((color) => {
+		colorHtml += `<div 
+                        class="color"
+                        style="background-color:${color};color:${color}"
+                        >${color}
+                        </div>`
+		namedColorHtml += `<div 
+                        class="named-color"
+                        >${color}
+                        </div>`
+	});
+	colorContainer.innerHTML = colorHtml
+	namedColorContainer.innerHTML = namedColorHtml
+    colors = []
 }
