a80e51bb4009d9c3fcee2422bdbacf2d375929cf
Fridrich Ferenc <sontii78@gmail.com>
2022-12-17 11:23:36 +0100

    .

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const colorBtn = document.getElementById('btn')
-const colorPicker = document.getElementById("input-color-picker")
-const colorschema = document.getElementById("color-schema-select");
+const colorPicker = document.getElementById('input-color-picker')
+const colorschema = document.getElementById('color-schema-select');
+const colorContainer = document.getElementById("color-container");
+const namedColorContainer = document.getElementById("named-color-container");
+
+const colors = []
 
 document.getElementById('wrapper').addEventListener('click', (e) => {
     if(e.target.id === 'btn') {
@@ -9,11 +13,25 @@ document.getElementById('wrapper').addEventListener('click', (e) => {
     }
 })
 
-function getApiData(colorValue, mode) {
-    
-    fetch(
-			`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${mode}`
-		)
-			.then((res) => res.json())
-			.then((data) => console.log(data));
+function getApiData(colorValue, mode) {   
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${mode}`)
+        .then(res => res.json())
+        .then(data => {
+                data.colors.map(hexValue => {
+                    colors.push(hexValue.hex.value)
+                })
+        });
+
+    render()
+}
+
+function render() {
+    let colorHtml = ''
+    let namedColorHtml = ''
+    colors.map(color => {
+        colorHtml += `<div class"color"style="background-color:˘${color};"></div>`;
+        namedColorHtml += `<div class"named-color">${color};" </div>`;
+    })
+    colorContainer = colorHtml
+    namedColorContainer = namedColorHtml
 }
